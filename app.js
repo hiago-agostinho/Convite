f1b40fe2bb37bac5190db5f44205b62a92926139
Hiago Agostinho <97325933+hiago-agostinho@users.noreply.github.com>
2023-04-25 22:02:04 -0300

    a

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
-// adicionar música à lista de reprodução
-function addToPlaylist() {
-    const videoId = document.getElementById('videoId').value;
-    const playlistId = 'https://www.youtube.com/playlist?list=PLEzcYxvLqJSOzKS9ExixleaOIOSz-zpNr';
-    const accessToken = 'AIzaSyAmD3rZxyCA5CzgOTijslCOZpzzcT3sHcA';
-    
-    // enviar solicitação HTTP POST para adicionar o vídeo à lista de reprodução
-    const xhr = new XMLHttpRequest();
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&access_token=${accessToken}`;
-    const data = {
+// configurações de autenticação
+const CLIENT_ID = '883128468855-l1pqhsi4pvrdnjked5m9pa224t9h16gv.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyAmD3rZxyCA5CzgOTijslCOZpzzcT3sHcA';
+const SCOPE = 'https://www.googleapis.com/auth/youtube.force-ssl';
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'];
+
+// inicialização da API
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPE
+  }).then(function() {
+    // ouvir o clique do botão Adicionar Música
+    document.getElementById('addMusicButton').addEventListener('click', addToPlaylist);
+  }, function(error) {
+    console.log(JSON.stringify(error, null, 2));
+  });
+}
+
+// autenticação do usuário
+function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+// enviar solicitação HTTP POST para adicionar o vídeo à lista de reprodução
+function addVideoToPlaylist(videoId, playlistId) {
+  var details = {
+    videoId: videoId,
+    kind: 'youtube#video'
+  }
+  var request = gapi.client.youtube.playlistItems.insert({
+    part: 'snippet',
+    resource: {
       snippet: {
         playlistId: playlistId,
-        resourceId: {
-          kind: 'youtube#video',
-          videoId: videoId
-        }
+        resourceId: details
       }
-    };
-    xhr.open('POST', url, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        alert('Música adicionada à playlist com sucesso!');
-      }
-      else {
-        alert('Houve um erro ao adicionar a música à playlist');
-      }
-    };
-    xhr.send(JSON.stringify(data));
-  }
+    }
+  });
+  request.execute(function(response) {
+    console.log(response);
+    alert('Música adicionada à playlist com sucesso!');
+  });
+}
+
+// adicionar música à lista de reprodução
+function addToPlaylist() {
+  const videoId = document.getElementById('videoId').value;
+  const playlistId = 'https://www.youtube.com/playlist?list=PLEzcYxvLqJSOzKS9ExixleaOIOSz-zpNr';
   
+  // enviar solicitação HTTP POST para adicionar o vídeo à lista de reprodução
+  addVideoToPlaylist(videoId, playlistId);
+}
+
+// carregar a API do cliente do YouTube e autenticar o usuário
+function loadClient() {
+  gapi.load('client:auth2', initClient);
+  gapi.auth2.init({
+    client_id: CLIENT_ID,
+    scope: SCOPE
+  }).then(function() {
+    // ouvir o clique do botão de autenticação
+    document.getElementById('authorize-button').addEventListener('click', handleAuthClick);
+  }, function(error) {
+    console.log(JSON.stringify(error, null, 2));
+  });
+}
+
+// carregar a API do cliente do YouTube
+gapi.load('client', loadClient);
